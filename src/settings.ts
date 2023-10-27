b56f00040fbb767facfd92c5442794129b27403e
Rene BIGOT <rene.bigot@reseau.sncf.fr>
2023-10-27 18:05:57 +0200

    Rename forked plugin

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,2 @@
-export const ACCESSORY_NAME = "z2m-thermostat";
-export const PLUGIN_NAME = "homebridge-z2m-thermostat";
+export const ACCESSORY_NAME = "z2m-thermostat-pilot-wire";
+export const PLUGIN_NAME = "homebridge-z2m-thermostat-pilot-wire";
